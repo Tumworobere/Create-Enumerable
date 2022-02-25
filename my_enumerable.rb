19ce4be56b56f6544f906668687cb1992e50be7e
Annah Tomoro <annaht.annah@gmail.com>
2022-02-25 12:09:39 +0300

    Update:my_enumerable.rb

---
+++ my_enumerable.rb
@@ -0,0 +1,34 @@
+module MyEnumerable
+    def all?
+      @list.each do |val|
+        return true unless block_given?
+  
+        block_test = yield(val)
+        return false unless block_test
+      end
+      true
+    end
+  
+    def any?
+      @list.each do |val|
+        return true unless block_given?
+  
+        block_test = yield(val)
+        return true if block_test
+      end
+      false
+    end
+  
+    def filter
+      return @list unless block_given?
+      return @list if @list.length.zero?
+  
+      result = []
+      @list.each do |val|
+        block_test = yield(val)
+        result.push(val) if block_test
+      end
+      result
+    end
+  end
+  
